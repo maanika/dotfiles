6a252cf8e8298498b40dd2b7cb06dc0600ed271d
maanika <kennethmaanika@gmail.com>
2026-02-18 23:17:55 +1030

    add external monitor support

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,13 +6,11 @@ killall -q polybar
 # Path to config
 CONFIG_FILE="$HOME/.config/polybar/config.ini"
 
-# Check if xrandr exists
-if type xrandr >/dev/null 2>&1; then
-    # Loop over all connected monitors
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload -c "$CONFIG_FILE" toph &
-    done
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+      MONITOR=$m polybar --reload -c "$CONFIG_FILE" toph &
+  done
 else
-    # Fallback: just launch one bar
-    polybar --reload -c "$CONFIG_FILE" toph &
+  polybar --reload example &
 fi
+
